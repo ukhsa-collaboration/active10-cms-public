e17c1a9bafacba91c2aa9f86f788e1d7154862e9
Sam Ainsworth <sam.ainsworth@ukhsa.gov.uk>
2026-03-13 11:07:59 +0000

    lint: ruff format

--- applications/articles/serializers.py
+++ applications/articles/serializers.py
@@ -28,7 +28,11 @@ class ArticleSerializer(serializers.ModelSerializer):
         return {"ios": ios, "android": android}
 
     def get_view_ids(self, obj):
-        return obj.views.through.objects.filter(article=obj.id).order_by("order").values_list("view_id", flat=True)
+        return (
+            obj.views.through.objects.filter(article=obj.id)
+            .order_by("order")
+            .values_list("view_id", flat=True)
+        )
 
     def get_related_article_ids(self, obj):
         return (
@@ -50,7 +54,11 @@ class ArticleSerializer(serializers.ModelSerializer):
         return html_text_to_json(obj.content)
 
     def get_content_view_ids(self, obj):
-        return ContentView.objects.filter(article__id=obj.id).order_by("order").values_list("view_id", flat=True)
+        return (
+            ContentView.objects.filter(article__id=obj.id)
+            .order_by("order")
+            .values_list("view_id", flat=True)
+        )
 
     class Meta:
         model = Article
