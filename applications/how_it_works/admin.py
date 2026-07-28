d9d7d6dce7bfbe918eb56c14c754bbdcc4ec4c9d
Mike Severs <mike.severs@flipsidegroup.com>
2026-07-28 11:26:54 +0100

    [A10N-2578] add sorting in admin for HowItWorks

--- applications/how_it_works/admin.py
+++ applications/how_it_works/admin.py
@@ -1,13 +1,14 @@
 from django.contrib import admin
+from adminsortable2.admin import SortableAdminMixin
 from applications.how_it_works.models import HowItWorks
 from utils.admin import CloneAdminMixin
 
 
-class HowItWorksAdmin(CloneAdminMixin, admin.ModelAdmin):
+class HowItWorksAdmin(CloneAdminMixin, SortableAdminMixin, admin.ModelAdmin):
     clone_name_field = "title"
-    fields = ["id", "title", "description", "image", "activity_type"]
-    readonly_fields = ["id"]
-    list_display = ["title", "activity_type"]
+    fields = ["id", "title", "description", "image", "list_order", "activity_type"]
+    readonly_fields = ["id", "list_order"]
+    list_display = ["list_order", "title", "activity_type"]
     list_filter = ["activity_type"]
 
 
